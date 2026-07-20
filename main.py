b15692bf2559a00669585a70cea8b16a4fb932f3
Elena Volkova <lenochka.volkova@gmail.com>
2026-07-20 15:22:44 +0400

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 # Create an app
 app = FastAPI()
+
+# Dictionary with tasks instead of db
+tasks_db = [
+    {"id": 1, "title": "Install FastAPI", "done": False},
+    {"id": 2, "title": "Create first endpoint", "done": True},
+    {"id": 3, "title": "Add tasks", "done": False}
+]
 
 # Configure root path - retutn API description
 @app.get("/")
 def read_root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
 
-#New endpoint to check that server is working
+#Endpoint to check that server is working
 @app.get("/health")
 def get_info():
     return { "status": "ok" }
+
+# Endpoint to get tasks list
+@app.get("/tasks")
+def get_tasks():
+    return tasks_db
+
+#Endpoint to get a task by id
+@app.get("/tasks/{task_id}")
+def get_task_by_id(task_id: int):
+    for task in tasks_db:
+        if task["id"] == task_id:
+            return task
+            
+    # If there is no task with such a number, then return 404 error code
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
 
 # Launch server 
 if __name__ == "__main__":
