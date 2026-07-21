cae824dccd870df49c5c28e497fa0caa4abb251f
Elena Volkova <lenochka.volkova@gmail.com>
2026-07-21 16:18:45 +0400

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, Field
+#from typing import Optional
 import uvicorn
 
 # Create an app
@@ -14,6 +15,13 @@ tasks_db = [
 
 class TaskCreate(BaseModel):
     title: str
+
+# Validation for update (PUT)
+class UpdateTaskModel(BaseModel):
+    # Field(..., min_length=1) is to ensure that the string is not empty.
+    # "Optional" allows to update "title" or "done", or both.
+    title: str | None = Field(None, min_length=1, description="Task's title cannot be empty")
+    done: bool | None = Field(None, description="Status of the task")
 
 # Configure root path - retutn API description
 @app.get("/")
@@ -85,6 +93,52 @@ def create_task(task_data: TaskCreate):
 #content-type: application/json
 #{"id":4,"title":"Buy milk","done":false}%
 
+
+### Endpoint PUT /tasks/{id}
+@app.put("/tasks/{id}")
+async def update_task(id: int, task_body: UpdateTaskModel):
+    task = next((t for t in tasks_db if t["id"] == id), None)
+    
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Task with id {id} not found"
+        )
+    
+    update_data = task_body.model_dump(exclude_unset=True)
+    if not update_data:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Body cannot be empty. Provide 'title' or 'done'."
+        )
+    
+    if "title" in update_data:
+        task["title"] = update_data["title"]
+    if "done" in update_data:
+        task["done"] = update_data["done"]
+        
+    return task
+
+
+### Endpoint DELETE /tasks/{id}
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(id: int):
+    task = next((t for t in tasks_db if t["id"] == id), None)
+    
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Task with id {id} not found"
+        )
+        
+    tasks_db.remove(task)
+    
+    # 4. Returns None (FastAPI creates response 204 "no content" automatically)
+    return None
+
+# in order to test PUT and DELETE following curl-strings can be used:
+# curl -X PUT "http://localhost:8000/tasks/4" -H "Content-Type: application/json" -d '{"title": "Byu milk and eggs", "done": true}'
+# curl -i -X DELETE http://localhost:8000/tasks/4 
 
 # Launch server 
 if __name__ == "__main__":
